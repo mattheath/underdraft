ba4b80719189ae8c563704ee32991eb00bb21a10
Jonas Huckestein <jonas.huckestein@gmail.com>
2014-10-25 13:27:11 +0100

    more fanciness

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,6 +1,9 @@
 class PositionsController < ApplicationController
   def show
     @projecting_overdraft = true
+    @amount = "£479"
+    @repay_amount = "£479.85"
+    @fee_in_p = "85"
   end
 
   def success
